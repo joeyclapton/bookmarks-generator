5c03363dcce224b2ab8b26c84e2a58fbf759e0c1
joeyclapton <joeyclapton42@gmail.com>
2023-07-31 15:43:32 -0300

    feat: adjust css

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,7 +1,9 @@
 'use client';
-import { Input, Spacer, Button, Textarea } from "@nextui-org/react";
-import { useState } from 'react';
-import { CameraIcon } from './CameraIcon';
+import { Input, Link, Button, Textarea, Loading, Badge, Row, Spacer, Col, Container } from "@nextui-org/react";
+import { useState, useRef } from 'react';
+
+import { RxDownload } from "react-icons/rx";
+
 
 import template from './utils/template';
 
@@ -12,6 +14,7 @@ export default function Form() {
 
   const [links, setLinks] = useState("");
   const [templateHtml, setTemplateHtml] = useState(template)
+  const downloadButtonRef = useRef<HTMLAnchorElement>(null);
 
   function onGenerateFolder() {
     const folderWithoutLinks = templateHtml.concat(`
@@ -20,7 +23,7 @@ export default function Form() {
     )
     setTemplateHtml(folderWithoutLinks)
 
-    for (var i = 0; i < links.length; i++) {
+    for (let i = 0; i < links.length; i++) {
 
       // // Skip blank lines
       // if (links[i] == "") {
@@ -32,7 +35,8 @@ export default function Form() {
       //   bookmarkNames[i] = links[i];
       // }
 
-      var bookmark = `<DT><A HREF="${links[i]}">${links[i]}</A>`
+      const bookmark = `<DT><A HREF="${links[i]}">${links[i]}</A>`
+
       setTemplateHtml(templateHtml.concat(bookmark))
     }
     console.log(templateHtml)
@@ -40,24 +44,52 @@ export default function Form() {
 
   }
 
+  function onDownload() {
+    const FILENAME = 'bookmarks.html'
+    const blob = new Blob([templateHtml], { type: 'text/plain' });
+
+    if (downloadButtonRef.current) {
+      downloadButtonRef.current.href = URL.createObjectURL(blob);
+      downloadButtonRef.current.download = FILENAME;
+    }
+
+  }
+
   return (
     <div>
-      <Input clearable label="Name" placeholder="Enter your folder name" initialValue="" value={folderName} onChange={(event: any) => setFolderName(event.target.value)}
-      />
-      <section>
-        <Textarea
-          label="Write your links"
-          placeholder="Enter your links."
-          onChange={(event: any) => {
-            const text = event.target.value.split('\n');
-            setLinks(text)
-            onGenerateFolder()
-          }}
-        />
-        <Button iconRight={<CameraIcon fill="currentColor" />}>
-          Take a photo
-        </Button>
-      </section>
+      <Container>
+        <div>
+          <Input clearable label="Folder name (Optional)" placeholder="Enter your folder name" initialValue="" value={folderName} onChange={(event: any) => setFolderName(event.target.value)}
+            css={{ width: "380px" }}
+          />
+        </div>
+        <div>
+          <Textarea
+            label="Links"
+            placeholder="Enter your links"
+            onChange={(event: any) => {
+              const text = event.target.value.split('\n');
+              setLinks(text)
+              onGenerateFolder()
+            }}
+            css={{ width: "380px" }}
+          />
+        </div>
+        <Row alignItems="center" justify="center">
+          <Button disabled bordered>
+            CSV
+            <Spacer />
+            <Badge size="xs">COMING SOON</Badge>
+          </Button>
+          <Spacer y={1} />
+          <Button iconRight={<RxDownload fill="currentColor" />} color="gradient" auto>
+            <Link href="#" css={{ color: "#ffffff" }} id="download" download ref={downloadButtonRef} onClick={onDownload}>
+              Download HTML
+            </Link>
+            {/* <Loading type="points" color="currentColor" size="sm" /> */}
+          </Button>
+        </Row>
+      </Container>
     </div>
   );
 }
